7dabd65c7df8cd90c8538b88c3ac2927786e7f00
Roman Golubev <gromniki@gmail.com>
2019-06-11 14:18:56 +0300

    Исправил замечания наставника

--- js/stat.js
+++ js/stat.js
@@ -9,8 +9,8 @@ var FONT_GAP = 20;
 var BAR_WIDTH = 40;
 var barHeight = CLOUD_HEIGHT - GAP - FONT_GAP - GAP;
 var barY = CLOUD_HEIGHT - GAP + FONT_GAP;
-var fontColor = '#000000';
-var barColorMe = 'rgba(255, 0, 0, 1)';
+var FONT_COLOR = '#000000';
+var USER_DEFINED_COLOR = 'rgba(255, 0, 0, 1)';
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
@@ -29,7 +29,7 @@ var getMaxElement = function (arr) {
   return maxElement;
 };
 
-var barColorOthers = function () {
+var getRandomColor = function () {
   return 'rgba(0, 0, 255, ' + Math.random() + ')';
 };
 
@@ -41,28 +41,21 @@ window.renderStatistics = function (ctx, players, times) {
   renderCloud(ctx, CLOUD_X + CLOUD_Y, CLOUD_Y * 2, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#ffffff');
 
-  ctx.fillStyle = fontColor;
+  ctx.fillStyle = FONT_COLOR;
   ctx.font = '16px PT Mono';
   ctx.fillText('Ура вы победили!', CLOUD_X + FONT_GAP, CLOUD_Y * 2 + FONT_GAP);
   ctx.fillText('Список результатов:', CLOUD_X + FONT_GAP, CLOUD_Y * 4 + FONT_GAP);
 
   var maxTime = getMaxElement(times);
 
-  var calculatedBarHeight = function (index) {
-    return barHeight * times[index] / maxTime;
-  };
-
   for (var i = 0; i < players.length; i++) {
+    var calculatedBarHeight = barHeight * times[i] / maxTime;
     ctx.fillText(players[i], CLOUD_X + FONT_GAP * 2 + (GAP + BAR_WIDTH) * i, CLOUD_HEIGHT - CLOUD_Y);
 
-    if (players[i] === 'Вы') {
-      ctx.fillStyle = barColorMe;
-    } else {
-      ctx.fillStyle = barColorOthers();
-    }
+    ctx.fillStyle = (players[i] === 'Вы') ? USER_DEFINED_COLOR : getRandomColor();
 
-    ctx.fillRect(getBarX(i), barY, BAR_WIDTH, -calculatedBarHeight(i));
-    ctx.fillStyle = fontColor;
-    ctx.fillText(Math.round(times[i]), getBarX(i), barY - calculatedBarHeight(i) - 10);
+    ctx.fillRect(getBarX(i), barY, BAR_WIDTH, -calculatedBarHeight);
+    ctx.fillStyle = FONT_COLOR;
+    ctx.fillText(Math.round(times[i]), getBarX(i), barY - calculatedBarHeight - 10);
   }
 };
