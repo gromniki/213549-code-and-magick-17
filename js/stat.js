2f59a0a58ee69e87720651f81b565d9a38fe2212
Roman Golubev <gromniki@gmail.com>
2019-07-10 22:02:54 +0400

    Мелкие правки

--- js/stat.js
+++ js/stat.js
@@ -20,13 +20,11 @@
 
   var getMaxElement = function (arr) {
     var maxElement = arr[0];
-
     for (var i = 0; i < arr.length; i++) {
       if (arr[i] > maxElement) {
         maxElement = arr[i];
       }
     }
-
     return maxElement;
   };
 
